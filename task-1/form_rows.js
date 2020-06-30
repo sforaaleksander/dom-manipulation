627b680a1d87d5c23af8748ace7e2a790e4081eb
Alek <sforaaleksander@gmail.com>
2020-06-30 19:11:04 +0200

    implement adding new rows to task-1

--- task-1/form_rows.js
+++ task-1/form_rows.js
@@ -1,5 +1,40 @@
+function createRow(){
+    console.log("create row replay!");
+    let rowNumber = findRowNumber() + 1;
+    console.log("creating row");
+    const label = document.createElement("label");
+    label.setAttribute("for", `item_${rowNumber}`);
+    label.innerHTML = `Item ${rowNumber}:`;
+
+    const input = document.createElement("input");
+    input.setAttribute("type", "text");
+    input.setAttribute("name", `item_${rowNumber}`);
+    input.setAttribute("id", `item_${rowNumber}`);
+
+    const form = document.createElement("form");
+    form.setAttribute("method", "GET");
+    form.appendChild(label);
+    form.appendChild(input);
+
+    document.querySelector(".container").appendChild(form);
+}
+
+function findRowNumber(){
+    // let innerHtml = document.querySelector('label:last-of-type').innerHTML;
+    // console.log(innerHtml);
+    // let rowNum = innerHtml.replace("Item ", "");
+    // rowNum = rowNum.replace(":","");
+    let elements = document.querySelectorAll('label');
+
+    return elements.length;
+}
+
 function main() {
-    // event handlers and other code here
+    console.log("clicked!");
+    let button = document.getElementById("add_row");
+    button.addEventListener('click', createRow);
 }
 
 main();
+ 
+
