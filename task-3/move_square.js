326f801b838a9f9e49258ace64e4b12155cbe26a
Alek <sforaaleksander@gmail.com>
2020-07-01 15:50:23 +0200

    task3: create grid

--- task-3/move_square.js
+++ task-3/move_square.js
@@ -1,15 +1,43 @@
 // use these keycodes for calling the appropriate functions
 // write your moveX functions and after that
 // you can call these like moves[keycode]();
-const moves = {
-    '97': moveLeft,
-    '115': moveDown,
-    '119': moveUp,
-    '100': moveRight
-};
+
+// const moves = {
+//     '97': moveLeft,
+//     '115': moveDown,
+//     '119': moveUp,
+//     '100': moveRight
+// };
+
+function createCell(index) {
+    const div = document.createElement("div");
+    div.setAttribute("class", "cell");
+    div.setAttribute("id", index);
+
+    div.innerHTML = "&nbsp;";
+
+    const container = document.querySelector(".container");
+    container.setAttribute("style", "display:flex; flex-wrap:wrap;");
+    container.appendChild(div);
+}
+
+function hundredCells() {
+    for (let i = 0; i < 100; i++) {
+        createCell(i);
+    }
+}
 
 function main() {
-    // event handlers and other code here
+    hundredCells()
+    // placeColor();
+
+}
+
+function placeColor(){
+    let randomNumber = Math.floor(Math.random() * 100);
+    const randomCell = document.getElementById(randomNumber);
+    randomCell.focus();
 }
 
 main();
+ 
