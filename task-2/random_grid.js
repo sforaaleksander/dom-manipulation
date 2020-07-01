4897653c236b83d12de0f44aff5cb1b09cd299d5
Alek <sforaaleksander@gmail.com>
2020-07-01 15:39:07 +0200

    task2:random_grid.js changes randomly color

--- task-2/random_grid.js
+++ task-2/random_grid.js
@@ -1,28 +1,36 @@
 function main() {
     hundredCells();
+    setTimeout(randomCellColorChange, 1000);
+    console.log("loop");
+}
+
+function randomCellColorChange() {
     const cells = document.querySelectorAll(".cell");
     for (let cell of cells) {
-        setTimeout(cell.setAttribute("style", "height: 3vh; width: 3vw; border: solid black 0.1em; background-color: green;"), 1000);
-        console.log("loop");
+        cell.style.backgroundColor = "#2980b9";
+    }
+    let randomNumber = Math.floor(Math.random() * 100);
+    const randomCell = document.getElementById(randomNumber);
+    randomCell.style.backgroundColor = "#e67e22";
+}
+
+
+function hundredCells() {
+    for (let i = 0; i < 100; i++) {
+        createCell(i);
     }
 }
 
 
-function hundredCells(){
-    for (let i=0;i<100;i++){
-        createCell();
-    }
-}
-
-
-function createCell() {
+function createCell(index) {
     const div = document.createElement("div");
     div.setAttribute("class", "cell");
-    div.setAttribute("style", "height: 3vh; width: 3vw; border: solid black 0.1em; background-color: blue;");
+    div.setAttribute("id", index);
+
     div.innerHTML = "&nbsp;";
-    
+
     const container = document.querySelector(".container");
-    container.setAttribute("style", "display:flex; flex-wrap:wrap; align-items:");
+    container.setAttribute("style", "display:flex; flex-wrap:wrap;");
     container.appendChild(div);
 }
 
